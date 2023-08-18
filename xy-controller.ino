1047a7626f67ec29be8dac01bd8e60a10acd0c30
rockomatthews <rob@robmatthe.ws>
2023-08-18 12:25:02 -0500

    added more generic adafruit touchscreen library

--- xy-controller.ino
+++ xy-controller.ino
@@ -1,55 +1,55 @@
 #include <Adafruit_HX8357.h>
-#include <Adafruit_STMPE610.h>
+#include <Adafruit_TouchScreen.h>
 #include <WiFi.h>
 #define SerialMon Serial
 #include <AppleMIDI_Debug.h>
-#include <AppleMIDI.h>  // Add the AppleMIDI library
+#include <AppleMIDI.h> // Add the AppleMIDI library
 
-char ssid[] = "ATT4K"; // your network SSID (name)
+char ssid[] = "ATT4K";        // your network SSID (name)
 char pass[] = "passpasspass"; // your network password (use for WPA, or use as key for WEP)
 
-// TFT display and touchscreen pins
-#define TFT_CS   5
-#define TFT_DC   4
-#define TFT_RST  2
-#define TS_CS    15
+// TFT display pins
+#define TFT_CS 5
+#define TFT_DC 4
+#define TFT_RST 2
+
+// Touchscreen pins (update these to match your wiring)
+#define YP 13
+#define XM 12
+#define YM 14
+#define XP 27
 
 const int DEBOUNCE_DELAY = 70; // the debounce time; increase if the output flickers
 
-// The display uses hardware SPI, plus TFT_RST and TFT_DC
+// Initialize the TFT display
 Adafruit_HX8357 tft = Adafruit_HX8357(TFT_CS, TFT_DC, TFT_RST);
-Adafruit_STMPE610 ts = Adafruit_STMPE610(TS_CS);
+
+// Initialize the touchscreen
+Adafruit_TouchScreen ts = Adafruit_TouchScreen(XP, YP, XM, YM, 300);
 
 // global variables to keep track of the previous position
 int previous_x = -1;
 int previous_y = -1;
-unsigned long lastDebounceTime = 0; // the last time the output pin was toggled
-unsigned long t0 = millis();
-int8_t isConnected = 0;
 
 APPLEMIDI_CREATE_DEFAULTSESSION_INSTANCE();
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
-  Serial.print("working");
+  Serial.print("working1");
   tft.begin();
   tft.fillScreen(HX8357_BLACK); // Set the background to black
   Serial.println();
-  
-  if (!ts.begin()) {
-    Serial.println("Couldn't start touchscreen controller");
-    while (1);
-  }
-  
-  Serial.print("working");
-  DBG_SETUP(115200);
-  Serial.print("Booting");
-  WiFi.begin(ssid, pass);
-  while (WiFi.status() != WL_CONNECTED) {
+
+  Serial.print("working2");
+  1 WiFi.begin(ssid, pass);
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print("Establishing connection to WiFi..");
   }
   Serial.print("Connected to network");
+
   Serial.println(F("OK, now make sure you an rtpMIDI session that is Enabled"));
   Serial.println(F("Add device named Arduino with Host"));
   Serial.println(WiFi.localIP());
@@ -64,22 +64,25 @@ void setup() {
 
   MIDI.begin();
 
-  AppleMIDI.setHandleConnected([](const APPLEMIDI_NAMESPACE::ssrc_t & ssrc, const char* name) {
-    isConnected++;
-    DBG(F("Connected to session"), ssrc, name);
-  });
-  AppleMIDI.setHandleDisconnected([](const APPLEMIDI_NAMESPACE::ssrc_t & ssrc) {
-    isConnected--;
-    DBG(F("Disconnected"), ssrc);
-  });
+  AppleMIDI.setHandleConnected([](const APPLEMIDI_NAMESPACE::ssrc_t &ssrc, const char *name)
+                               {
+        isConnected++;
+        DBG(F("Connected to session"), ssrc, name); });
+  AppleMIDI.setHandleDisconnected([](const APPLEMIDI_NAMESPACE::ssrc_t &ssrc)
+                                  {
+        isConnected--;
+        DBG(F("Disconnected"), ssrc); });
 }
 
-void loop() {
+void loop()
+{
   MIDI.read();
 
-  if (ts.touched()) {
+  // Check if the screen is touched
+  TSPoint p = ts.getPoint();
+  if (p.z > ts.pressureThreshhold)
+  {
     Serial.println("Screen is pressed");
-    TS_Point p = ts.getPoint();
     int cc11 = map(p.x, 0, tft.width(), 0, 127);
     int cc10 = map(p.y, 0, tft.height(), 0, 127);
 
@@ -92,18 +95,22 @@ void loop() {
     tft.fillCircle(p.x, p.y, 10, HX8357_BLUE);
 
     // Draw a tail from the previous position to the current position
-    if (previous_x != -1 && previous_y != -1) {
+    if (previous_x != -1 && previous_y != -1)
+    {
       tft.drawLine(previous_x, previous_y, p.x, p.y, HX8357_BLUE);
     }
 
     // update the previous position
     previous_x = p.x;
-    previous_y = p.y;   
-
-  } else {
+    previous_y = p.y;
+  }
+  else
+  {
     MIDI.sendControlChange(66, 0, 5);
     // reset previous position
     previous_x = -1;
     previous_y = -1;
   }
+
+  // ... other code ...
 }
